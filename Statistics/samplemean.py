580e0ec1a17def5eae44fec7a9132ae0dcca5626
jay71294-dev <jpatels71294@gmail.com>
2019-11-17 12:29:06 -0500

    Feature:Added getsample.py file and added definition of sample mean

--- Statistics/samplemean.py
+++ Statistics/samplemean.py
@@ -1,6 +1,17 @@
-from random import random
+from Calculator.addition import addition
+from Calculator.division import division
+from Statistics.getsample import getSample
 
 
-def getSample(data, sample_size):
-    random_values = random.sample(data, k=sample_size)
-    return random_values
+def sample_mean(data, sample_size):
+    total = 0
+    # check that get sample returns the proper number of samples
+    # check that sample size is not 0
+    # check that sample size is not larger than the population
+    # https://realpython.com/python-exceptions/
+    # https://stackoverflow.com/questions/129507/how-do-you-test-that-a-python-function-throws-an-exception
+    sample = getSample(data, sample_size)
+    num_values = len(sample)
+    for num in sample:
+        total = addition(total, num)
+    return division(total, num_values)
