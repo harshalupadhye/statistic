371e7bb7abf3700033ce7ac86bb5219c78822509
hindl <hu23@njit.edu>
2019-11-16 22:20:59 -0500

    feature: added method named mode

--- Statistics/statistics.py
+++ Statistics/statistics.py
@@ -1,7 +1,7 @@
 from Calculator.calculator import Calculator
 from Statistics.mean import mean
-#from Statistics.median import median
-#from Statistics.mode import mod
+from Statistics.median import median
+from Statistics.mode import mod
 #from Statistics.Psd import psd
 #from Statistics.Vpp import vpp
 from CsvReader.CsvReader import CsvReader
@@ -20,19 +20,19 @@ class Statistics(Calculator):
         self.result = mean(d)
         return self.result
 
-    #def median(self):
-#    d = []
-    #    for row in self.data.data:
-    #        d.append(row['v'])
-    #    self.result = median(d)
-    #    return self.result
+    def median(self):
+        d = []
+        for row in self.data.data:
+           d.append(row['v'])
+        self.result = median(d)
+        return self.result
 
-    #def mod(self):
-#    d = []
-    #    for row in self.data.data:
-    #        d.append(row['v'])
-    #    self.result = mod(d)
-    #    return self.result
+    def mod(self):
+        d = []
+        for row in self.data.data:
+             d.append(row['v'])
+        self.result = mod(d)
+        return self.result
 
     #def psd(self):
     #    d = []
