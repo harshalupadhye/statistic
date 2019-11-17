70ce73b51f3ef8b579bbc449cb6290222f5eea7d
jay71294-dev <jpatels71294@gmail.com>
2019-11-17 15:20:19 -0500

    feature:Added sample standard dev. definition

---
+++ Statistics/sample_standdev.py
@@ -0,0 +1,15 @@
+=def samplestand(data,simple_size):
+
+    dev=0
+
+    sample_values=len(sample)
+
+    x_bar=mean()
+    x=sample_values
+    n=subtract(sample_values,1)
+    for dev in sample:
+        dev=subtraction(x,x_bar)
+        sqaure_x_bar=square(dev)
+        add=addition(sqaure_x_bar,sqaure_x_bar)
+        result=division(add,n)
+    return squareroot(result)
